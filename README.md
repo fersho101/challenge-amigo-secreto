afa77d6c3aa5f092c246416a45098644a3f5cadd
fernando corral <146652466+fersho101@users.noreply.github.com>
2025-02-01 22:22:11 -0700

    :art:

--- README.md
+++ README.md
@@ -5,6 +5,12 @@
 
 Implemetación del popular juego "Amigo Secreto", con javascript basico, html y css.
 
+## 📏 Forma de jugar
+
+- El jugador captura nombres de sus amigos en una lista de uno en uno con el boton añadir.
+
+- El programa seleciona un nombre del amigo secreto de manera aleatoria.
+
 ## ⚙️ Funciones
 
 - agregarAmigo(), Añade amigos a la lista de amigos.
@@ -15,6 +21,12 @@ Implemetación del popular juego "Amigo Secreto", con javascript basico, html y 
 
 - dezplegarLista(), dezpliega y muestra la lista de amigos añadidos
 
-- eliminaNombresDuplicados([nom]), no permite ingresar nombres duplicados o ya incluidos.
+- eliminaNombresDuplicados([nom, array]), no permite ingresar nombres duplicados o ya incluidos.
+
+- normalizarCadena([str]), normaliza una cadena pasada por parametro.
+
+- sortearAmigo(), selecciona un nombre de amigo de manera aleatoria o el destino los unio 😍.
 
 ## 🔗 Enlace
+
+<https://github.com/fersho101/challenge-amigo-secreto.git>
--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 
 let amigosArr = []
 let amigo = ''
+let listaAmigos = ''
 
+// Agrega nombre de amigo a la lista
 const agregarAmigo = () => {
     let nuevoAmigo = document.getElementById('amigo').value
     nuevoAmigo = validarNombre(nuevoAmigo)
-    nuevoAmigo = eliminaNombresDuplicados(nuevoAmigo)
+    nuevoAmigo = eliminaNombresDuplicados(nuevoAmigo, amigosArr)
+
     let resultado = document.getElementById('resultado')
     resultado.innerHTML = ''
 
@@ -14,34 +17,60 @@ const agregarAmigo = () => {
         amigosArr.push(nuevoAmigo)
         limpiarInput()
         dezplegarLista()
-        // console.log(amigosArr)
+        console.log(amigosArr)
     }
 }
 
+// Valida nombres validos
 const validarNombre = amigo => {
     const regex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+(?: [a-zA-ZáéíóúÁÉÍÓÚñÑ]+)*$/
-    amigo = amigo.trim()
+    amigo = amigo.trim() // Elimina espacios vacios al princio y al final
 
-    amigo = amigo.replace(/[^a-zA-ZáéíóúÁÉÍÓÚñÑ\s]/g, '')
-    amigo = amigo.replace(/\s+/g, ' ')
+    amigo = amigo.replace(/[^a-zA-ZáéíóúÁÉÍÓÚñÑ\s]/g, '') // elimina caracteres invalidos.
+    amigo = amigo.replace(/\s+/g, ' ') // Elimina espacios mayores a uno entre palabras.
 
-    if (regex.test(amigo)) {
-        return amigo
-    } else {
+    if (amigo.length < 2) {
         alert(
-            'Ingrese un nombre de amigo valido, solo se permiten letras, con o sin acento.'
+            'El nombre debe tener al menos 2 caracteres válidos. Solo se permiten letras, con o sin acento. Caracteres inválidos se eliminarán del nombre.'
         )
         limpiarInput()
+        return null
+    } else {
+        if (regex.test(amigo)) {
+            return amigo
+        }
         return null
     }
 }
 
+//Valida nombres duplicados
+const eliminaNombresDuplicados = (nom, arr) => {
+    nomNormalizado = normalizarCadena(nom)
+    if (arr.some(elem => normalizarCadena(elem) === nomNormalizado)) {
+        alert('Nombre de amigo ya incluido, agregue un nuevo')
+        limpiarInput()
+        return null
+    } else {
+        return nom
+    }
+}
+
+//Normalizar cadena
+const normalizarCadena = cadena => {
+    return cadena
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .toLowerCase()
+}
+
+//Limpia input
 const limpiarInput = () => {
     document.getElementById('amigo').value = ''
 }
 
+//Desplega lista de amigos capturados
 const dezplegarLista = () => {
-    let listaAmigos = document.getElementById('listaAmigos')
+    listaAmigos = document.getElementById('listaAmigos')
 
     listaAmigos.innerHTML = ''
     amigosArr.forEach(amigo => {
@@ -49,12 +78,36 @@ const dezplegarLista = () => {
     })
 }
 
-const eliminaNombresDuplicados =  amigo => {
-    if(amigosArr.includes(amigo)) {
-        alert('Nombre de amigo ya incluido, por favor agrege un nombre de amigo nuevo.')
-        limpiarInput()
-        return null
-    } else {
-        return amigo
+//Sortea amigo
+const sortearAmigo = () => {
+    //Comprueba arrglo no vacio
+    if (amigosArr.length === 0) {
+        alert(
+            'Lista de amigos vacía. Agregue al menos 1 nombre... aunque ya conocerás el resultado 😒.'
+        )
+        return
     }
+
+    let amigoSecreto = amigosArr[Math.floor(Math.random() * amigosArr.length)]
+
+    let resultado = document.getElementById('resultado')
+    listaAmigos.innerHTML = ''
+
+    resultado.innerHTML = `Amigo Secreto: ${amigoSecreto}`
+    amigosArr = []
+
+    document.getElementById('btn-sortear').style.display = 'none'
+    document.getElementById('btn-reiniciar').style.display = 'flex'
+    return
+}
+
+//Reiniciar juego
+const reiniciarJuego = () => {
+    document.getElementById('btn-reiniciar').style.display = 'none'
+    document.getElementById('btn-sortear').style.display = 'flex'
+    document.getElementById('resultado').innerHTML = ''
+
+    document
+        .getElementById('btn-reiniciar')
+        .addEventListener('click', reiniciarJuego)
 }
